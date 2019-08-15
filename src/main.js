4fea99c975ad348054309016eb4cfe8c38517503
lalalazero <zzzero520@hotmail.com>
2019-08-15 22:11:46 +0800

    修改组件驼峰命名

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import App from './App'
-import Button from './components/Button'
-import Icon from './components/Icon'
+import Button from './components/button'
+import Icon from './components/icon'
 import router from './router'
 import store from './store'
 
