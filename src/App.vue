c9f2d61ebf313a1fe56cfbc18749d3e9aa047ef4
lalalazero <zzzero520@hotmail.com>
2019-08-15 22:31:51 +0800

    添加 button-group 组件

--- src/App.vue
+++ src/App.vue
@@ -1,50 +1,44 @@
 <template>
   <div id="app">
     <z-button :loading="loading1" @click="click1">按钮</z-button>
-    <z-button icon='setting' :loading="loading2" 
-      @click="click2">
-      按钮
-    </z-button>
-    <z-button icon='setting' position='right' 
-      @click="click3"
-      :loading="loading3">按钮</z-button>
-    <z-button icon='down' :loading="false">
-      按钮
-    </z-button>
-    <z-button icon='like' position='right'>按钮</z-button>
+    <z-button icon="setting" :loading="loading2" @click="click2">按钮</z-button>
+    <z-button icon="setting" position="right" @click="click3" :loading="loading3">按钮</z-button>
+    <z-button-group>
+      <z-button icon="down" :loading="false">按钮</z-button>
+      <z-button icon="like" position="right">按钮</z-button>
+    </z-button-group>
   </div>
 </template>
 
 <script>
 export default {
   name: "App",
-  data(){
+  data() {
     return {
       loading1: true,
       loading2: true,
-      loading3: true,
-    }
+      loading3: true
+    };
   },
   methods: {
-    click1(){
-      this.loading1 = !this.loading1
-      console.log('clicked 1 ...')
+    click1() {
+      this.loading1 = !this.loading1;
+      console.log("clicked 1 ...");
     },
-    click2(){
-      this.loading2 = !this.loading2
-      console.log('clicked 2 ...')
+    click2() {
+      this.loading2 = !this.loading2;
+      console.log("clicked 2 ...");
     },
-    click3(){
-      this.loading3 = !this.loading3
-      console.log('clicked 3 ...')
+    click3() {
+      this.loading3 = !this.loading3;
+      console.log("clicked 3 ...");
     }
   }
 };
 </script>
 
 <style lang="scss">
-  #app {
-    margin: 20px;
-  }
-  
+#app {
+  margin: 20px;
+}
 </style>>
--- src/main.js
+++ src/main.js
@@ -2,12 +2,14 @@ import Vue from 'vue'
 import App from './App'
 import Button from './components/button'
 import Icon from './components/icon'
+import ButtonGroup from './components/button-group'
 import router from './router'
 import store from './store'
 
 // 注册为 Vue 的全局组件
 Vue.component('z-button', Button)
 Vue.component('z-icon',Icon)
+Vue.component('z-button-group',ButtonGroup)
 
 Vue.config.productionTip = false
 
