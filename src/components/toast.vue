e6332e0ac4c8103706365ed055d6a8574bf7da2a
lalalazero <zzzero520@hotmail.com>
2019-08-31 23:03:50 +0800

    完成 toast 初步样式和功能

--- src/components/toast.vue
+++ src/components/toast.vue
@@ -1,13 +1,31 @@
 <template>
-    <div class="toast">
-
-    </div>
+  <div class="toast">
+    <slot></slot>
+  </div>
 </template>
 <script>
 export default {
-    name: 'zViewToast'
-}
+  name: "zViewToast"
+};
 </script>
 <style lang="scss" scoped>
-
+.toast {
+    $toast-bg: rgba(0,0,0, 0.75);
+    $font-size: 14px;
+    $toast-height: 40px;
+    // border: 1px solid red;
+    position: fixed;
+    top: 0;
+    left: 50%;
+    transform: translateX(-50%);
+    padding: 0 16px;
+    color: white;
+    background: $toast-bg;
+    height: $toast-height;
+    font-size: $font-size;
+    line-height: 1.8;
+    border-radius: 4px;
+    display: flex;
+    align-items: center;
+}
 </style>
--- src/plugin.js
+++ src/plugin.js
@@ -1,14 +1,12 @@
+import Toast from './components/toast'
 export default {
     install(Vue, option){
         Vue.prototype.$toast = function(message){
-            // test if it work
-            console.log(' I am toast ~~')
-            // 用 alert 假装 toast
-            // alert(message)
-            // 用 div (而不是 Vue 组件) 假装 toast
-            let div = document.createElement('div')
-            div.textContent = message
-            document.body.appendChild(div)
+            const Constructor = Vue.extend(Toast)
+            const toast = new Constructor()
+            toast.$slots.default = [message]
+            toast.$mount()
+            document.body.appendChild(toast.$el)
         }
     }
 }
