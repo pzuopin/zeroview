31c4dd3f5ce8f1aee795ab8aa450a34a75620b91
lalalazero <zzzero520@hotmail.com>
2019-10-27 16:49:34 +0800

    test: 添加 popover 第二个测试用例（hover测试，还未完成）

--- tests/unit/popover.spec.js
+++ tests/unit/popover.spec.js
@@ -12,17 +12,45 @@ describe('Popover.vue', ()=>{
         expect(Popover).to.exist
     })
 
+    xit('hover 弹出内容', (done) => {
+        Vue.component('z-popover', Popover)
+        const div = document.createElement('div')
+        document.body.appendChild(div)
+        div.innerHTML = `
+        <z-popover ref='popover'>
+            <template slot="content">
+            <div>popover内容</div>
+            </template>
+            <button>点我</button>
+        </z-popover>
+        `
+        const vm = new Vue({
+            el: div
+        })
+
+        vm.$nextTick(()=>{
+            let event = new Event('mouseenter')
+            vm.$el.querySelector('button').dispatchEvent(event);
+            vm.$nextTick(()=>{
+                const { contentWrapper } = vm.$refs.popover.$refs 
+                console.log('mouseenter..')
+                console.log(contentWrapper)
+                done()
+            })
+        })
+    })
+
     it('接受 trigger , position 属性', (done)=>{
         Vue.component('z-popover', Popover)
         const div = document.createElement('div')
         document.body.appendChild(div)
         div.innerHTML = `
         <z-popover position='bottom' trigger='click' ref='popover'>
-        <template slot="content">
-          <div>popover内容</div>
-        </template>
-        <button>点我</button>
-      </z-popover>
+            <template slot="content">
+            <div>popover内容</div>
+            </template>
+            <button>点我</button>
+        </z-popover>
         `
         const vm = new Vue({
             el: div
