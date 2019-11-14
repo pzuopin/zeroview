70faf532df1f9ca89ad68e1b9448bda825dd0986
lalalazero <zzzero520@hotmail.com>
2019-11-14 18:28:26 +0800

    参照 element 魔改 md-loader.js 初步实现基本组件展示和代码 highlight

--- md-loader/containers.js
+++ md-loader/containers.js
@@ -10,10 +10,16 @@ module.exports = md => {
       if (tokens[idx].nesting === 1) {
         const description = m && m.length > 1 ? m[1] : '';
         const content = tokens[idx + 1].type === 'fence' ? tokens[idx + 1].content : '';
-        return `<demo-block>
+        const source = tokens[idx + 2].type === 'fence' ? tokens[idex + 2].content : 'source'
+        if (source === 'source') {
+          return `<demo-block>
         ${description ? `<div>${md.render(description)}</div>` : ''}
-        <!--element-demo: ${content}:element-demo-->
+        <div><p>xxxx</p>${content}</div>
         `;
+        }else {
+          return `<demo-block><template slot='source'>${content}</template></demo-block>`
+        }
+
       }
       return '</demo-block>';
     }
--- src/demo-block.vue
+++ src/demo-block.vue
@@ -4,12 +4,24 @@
       <slot name="source"></slot>
     </div>
     <div class="highlight">
-      <slot name='highlight'></slot>
+      <slot name="highlight"></slot>
     </div>
   </div>
 </template>
 <script>
+import hljs from "highlight.js";
 export default {
-  name: 'DemoBlock'
-}
+  name: "DemoBlock",
+  mounted() {
+    document.querySelectorAll("pre code").forEach(block => {
+      // hljs.highlightBlock(block);
+    });
+  }
+};
 </script>
+<style lang="scss" scoped>
+.highlight {
+  border: 1px solid #ccc;
+  padding: 20px;
+}
+</style>
