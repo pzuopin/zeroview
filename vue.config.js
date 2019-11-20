22ce04ee8f27cc4dd8c26fc4387f6cea26ad50cb
lalalazero <zzzero520@hotmail.com>
2019-11-20 22:35:41 +0800

    add cross-env

--- vue.config.js
+++ vue.config.js
@@ -1,32 +1,34 @@
-const path = require('path')
+const path = require("path");
+const example = process.env.EXAMPLE_ENV;
 module.exports = {
+  publicPath: example === "true" ? "/zeroview/" : "/",
   runtimeCompiler: true, // 使用包含运行时+编译器的vue构建版本,
   pluginOptions: {
-    'style-resources-loader': {
-      preProcessor: 'scss',
-      patterns: [
-        path.resolve(__dirname, './src/style.scss'),
-      ]
+    "style-resources-loader": {
+      preProcessor: "scss",
+      patterns: [path.resolve(__dirname, "./src/style.scss")]
     }
   },
   chainWebpack: config => {
     config.module
-      .rule('markdown')
+      .rule("markdown")
       .test(/\.md$/)
-      .use('vue-loader')
-      .loader('vue-loader').tap(options => {
+      .use("vue-loader")
+      .loader("vue-loader")
+      .tap(options => {
         return Object.assign({}, options, {
           compilerOptions: {
             preserveWhitespace: false
           }
-        })
+        });
       })
-      .end()
+      .end();
 
     config.module
-      .rule('markdown')
-      .test(/\.md$/).use(require.resolve('./md-loader.js'))
-      .loader(require.resolve('./md-loader.js'))
-      .end()
+      .rule("markdown")
+      .test(/\.md$/)
+      .use(require.resolve("./md-loader.js"))
+      .loader(require.resolve("./md-loader.js"))
+      .end();
   }
-}
+};
