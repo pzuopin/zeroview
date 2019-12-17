db952681c4f7a46c23d8cc226e92137b1e42626a
lalalazero <zzzero520@hotmail.com>
2019-12-17 20:00:11 +0800

    添加递归组件 cascader-item

---
+++ src/components/cascader/cascader-item.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="z-view-cascader-item">
+    {{ item.label }}
+    <div v-if="item.children">
+      <z-view-cascader-item :key="index" v-for="(child,index) in item.children" :item="child"></z-view-cascader-item>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "zViewCascaderItem",
+  props: {
+    item: {
+      type: Object
+    }
+  }
+};
+</script>
+<style lang="scss" scoped>
+.z-view-cascader-item {
+  border: 1px solid blue;
+  margin: 10px;
+}
+</style>
--- src/components/cascader/cascader.vue
+++ src/components/cascader/cascader.vue
@@ -1,17 +1,17 @@
 <template>
   <div class="z-view-cascader">
     <div :key="index" v-for="(item,index) in options">
-      {{ item.label }}
-      <div :key="index1" v-for="(item1,index1) in item.children">
-        {{ item1.label }}
-        <div :key="index2" v-for="(item2,index2) in item1.children">{{ item2.label }}</div>
-      </div>
+      <z-view-cascader-item :item="item"></z-view-cascader-item>
     </div>
   </div>
 </template>
 <script>
+import zViewCascaderItem from "./cascader-item.vue";
 export default {
   name: "zViewCascader",
+  components: {
+    zViewCascaderItem
+  },
   props: {
     options: {
       type: Array
@@ -21,7 +21,7 @@ export default {
 </script>
 <style lang="scss" scoped>
 .z-view-cascader {
-  border: 1px solid red;
+  //   border: 1px solid red;
   margin: 10px;
 }
 </style>
